348770d4aed112f20fe9c7a32c9149a49effbf4c
Teodor <teodor.pripoae@gmail.com>
2023-11-28 01:07:54 +0200

    feat: add util.RandomAlphaNumericString

---
+++ util/random.go
@@ -0,0 +1,15 @@
+package util
+
+import (
+	"math/rand"
+)
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+func RandomAlphaNumericString(length int) string {
+	b := make([]rune, length)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
